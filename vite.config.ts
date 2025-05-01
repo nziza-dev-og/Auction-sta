ddb19385798cbac109d01541000a386a76be071a
nziza-dev-og <nzizadev@gmail.com>
2025-05-01 23:58:45 +0200

    Update vite.config.ts
    new base

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // or '/' depending on your routing setup
   plugins: [react()],
   optimizeDeps: {
     include: ['lucide-react'],
